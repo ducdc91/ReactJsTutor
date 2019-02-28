f32d857d47f54549ac9727dcff43b1e832d0558e
Ducdc <ducdc91@gmail.com>
2019-02-28 13:57:27 +0700

    Updates

---
+++ precache-manifest.ceb0d9b8865af5ce4ce535f251ab29d4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e4bc5c2ac2d73ecaf46",
+    "url": "/ReactJsTutor/static/css/main.96c46893.chunk.css"
+  },
+  {
+    "revision": "9e4bc5c2ac2d73ecaf46",
+    "url": "/ReactJsTutor/static/js/main.9e4bc5c2.chunk.js"
+  },
+  {
+    "revision": "b9abe0e908b05e698efd",
+    "url": "/ReactJsTutor/static/js/runtime~main.b9abe0e9.js"
+  },
+  {
+    "revision": "838835256c241f9b6b64",
+    "url": "/ReactJsTutor/static/js/2.83883525.chunk.js"
+  },
+  {
+    "revision": "5747ab3cf6c6ad96b2acbd275431859d",
+    "url": "/ReactJsTutor/static/media/avatar.5747ab3c.JPEG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJsTutor/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "65779ebcc35604a25c2ba77309c5b8af",
+    "url": "/ReactJsTutor/static/media/fa-regular-400.65779ebc.woff2"
+  },
+  {
+    "revision": "5dd3976cb5d61e2e561f2a46b916f377",
+    "url": "/ReactJsTutor/static/media/fa-regular-400.5dd3976c.woff"
+  },
+  {
+    "revision": "414ff5daad323a1c47c5177d4bd29674",
+    "url": "/ReactJsTutor/static/media/fa-regular-400.414ff5da.eot"
+  },
+  {
+    "revision": "f6c6f6c8cb7784254ad00056f6fbd74e",
+    "url": "/ReactJsTutor/static/media/fa-regular-400.f6c6f6c8.ttf"
+  },
+  {
+    "revision": "4b115e1153a9ea339d6a0bb284cc8ed3",
+    "url": "/ReactJsTutor/static/media/fa-brands-400.4b115e11.woff2"
+  },
+  {
+    "revision": "462806316fea535a6a57651bc2b000b0",
+    "url": "/ReactJsTutor/static/media/fa-solid-900.46280631.woff2"
+  },
+  {
+    "revision": "b90365bccdabd68c6c03902b4b141f09",
+    "url": "/ReactJsTutor/static/media/fa-brands-400.b90365bc.woff"
+  },
+  {
+    "revision": "61969d433bf265b9717a6c357a1e04e4",
+    "url": "/ReactJsTutor/static/media/fa-solid-900.61969d43.woff"
+  },
+  {
+    "revision": "d9d17590c975aad1be0ddab673f9c769",
+    "url": "/ReactJsTutor/static/media/fa-brands-400.d9d17590.eot"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/ReactJsTutor/static/media/fa-brands-400.c39278f7.ttf"
+  },
+  {
+    "revision": "e7e957c87c454bccaa3bf9fdaa6874f8",
+    "url": "/ReactJsTutor/static/media/fa-regular-400.e7e957c8.svg"
+  },
+  {
+    "revision": "b5596f4d339f99e3d69bc41be78db962",
+    "url": "/ReactJsTutor/static/media/fa-solid-900.b5596f4d.eot"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/ReactJsTutor/static/media/fa-solid-900.b70cea03.ttf"
+  },
+  {
+    "revision": "80533988ff5fecd5be26557d08ce8237",
+    "url": "/ReactJsTutor/static/media/fa-brands-400.80533988.svg"
+  },
+  {
+    "revision": "82905d8d1c06969df11c8c378e9bdd4c",
+    "url": "/ReactJsTutor/static/media/fa-solid-900.82905d8d.svg"
+  },
+  {
+    "revision": "df14e8e1343e9e9cd53e9295ae8ec65f",
+    "url": "/ReactJsTutor/index.html"
+  }
+];
